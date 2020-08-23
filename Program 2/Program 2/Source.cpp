888a6dfbd7331ccb7de40fbcacb0da54331c0efe
pondonwanya <63011060@kmitl.ac.th>
2020-08-23 21:11:21 +0700

    add committt

--- Program 2/Program 2/Source.cpp
+++ Program 2/Program 2/Source.cpp
@@ -2,13 +2,15 @@
 #include <stdio.h>
 int main()
 {
-	int num, ber, a, b;
-	printf ("Enter first number :");
+	int num, ber, a, gcd;
+	printf("Enter first number :");
 	scanf("%d", &num);
-	printf ("Enter second number :");
+	printf("Enter second number :");
 	scanf("%d", &ber);
 	for (a = 1; (a <= num) && (a <= ber); a++)
+	{
 		if ((num % a == 0) && (ber % a == 0))
-			b = a;
-	printf("Greatest common divisor = %d", b);
+			gcd = a;
+	}
+	printf("Greatest common divisor = %d",gcd);
 }
